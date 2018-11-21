a92f0ea31bdd1674a95d930e34ca7269e268ba7a
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-21 20:38:14 +0900

    変数を利用して typeの型や初期値の確認をする

--- src/variables/variables.go
+++ src/variables/variables.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/cmplx"
 	"reflect"
 )
 
 // boolは boolean型
 var c, python, java bool
+
+var (
+	ToBe bool = false
+	// 絶対値-> uint , int -> 整数
+	// float -> 浮動小数点
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	s      string
+)
 
 func main() {
 	// int数字を扱う型であり、変数名 i を宣言する
@@ -24,4 +35,18 @@ func main() {
 	var s = 10000
 	fmt.Println(s)                 // OK!
 	fmt.Println(reflect.TypeOf(s)) // OK! #=> int型
+
+	fmt.Println("--- typeの変換 ---")
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	fmt.Println("--- basic type の確認 ---")
+	fmt.Printf("Type: %T value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T value: %v\n", z, z)
+
+	fmt.Println("string型の初期値")
+	fmt.Printf("%q\n", s)
 }
