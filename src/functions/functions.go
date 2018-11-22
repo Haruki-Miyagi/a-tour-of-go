1f35d62b93ffa15abf574234266cf8e146b1a67a
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-23 00:15:50 +0900

    Add 関数の返り値について

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -9,6 +9,18 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func sub(x int, y int) int {
+	return x - y
+}
+
+func swap(x, y string) (string, string) {
+	return x, y
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	fmt.Println(sub(42, 13))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
