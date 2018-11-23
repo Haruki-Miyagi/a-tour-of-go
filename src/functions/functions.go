0b1a7a06aa89cd8d7d2555e27946d350e9ffb241
Haruki Miyagi <haruki.miyagi1112@gmail.com>
2018-11-23 13:02:21 +0900

    関数での return の使い方

--- src/functions/functions.go
+++ src/functions/functions.go
@@ -17,10 +17,23 @@ func swap(x, y string) (string, string) {
 	return x, y
 }
 
+func add_square(x, y int) int {
+	x = x + y // 4
+	s := 2
+	// return は必要
+	return square(x, s)
+}
+
+func square(x, s int) int {
+	return x * s // 8
+}
+
 func main() {
 	fmt.Println(add(42, 13))
 	fmt.Println(sub(42, 13))
 
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
+
+	fmt.Println(add_square(1, 3))
 }
